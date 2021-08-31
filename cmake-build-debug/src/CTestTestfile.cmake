731ec3a19318cb97e2d5b86afdddb1890213ef29
Benjamin Hennies <benjaminhennies@posteo.de>
2021-08-31 11:34:08 +0200

    remove build dir

--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benjamin/Dokumente/Uni/Hiwi_TCS/TMRexp-POPL19-chksmr/src
-# Build directory: /home/benjamin/Dokumente/Uni/Hiwi_TCS/TMRexp-POPL19-chksmr/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/test/EBR/CMakeFiles/EBRimpl_1Set.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for EBRimpl_1Set.
-# This may be replaced when dependencies are built.
